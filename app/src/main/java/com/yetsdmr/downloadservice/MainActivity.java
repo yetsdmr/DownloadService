c8685e3b1f61809295551058755f02d781914b0a
EMRE TAŞDEMİR <yetsdmr@gmail.com>
2020-08-27 11:40:19 +0300

    Initial commit

---
+++ app/src/main/java/com/yetsdmr/downloadservice/MainActivity.java
@@ -0,0 +1,26 @@
+package com.yetsdmr.downloadservice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void download(View view){
+        Intent intent = new Intent(this,DownloadService.class);
+        startService(intent);
+    }
+
+    public void stop(View view){
+        Intent intent = new Intent(this,DownloadService.class);
+        stopService(intent);
+    }
+}
